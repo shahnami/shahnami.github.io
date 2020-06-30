c85031378a108b3ec15c0c2d6d418e1dca05fa14
Nami Shah <shah.nami@hotmail.com>
2020-06-30 21:48:57 +0100

    Updates

--- next.config.js
+++
@@ -1,9 +0,0 @@
-const debug = process.env.NODE_ENV !== 'production';
-const withImages = require('next-images');
-
-module.exports = withImages({
-	assetPrefix: !debug ? '' : '',
-	webpack(config, options) {
-		return config;
-	}
-});
