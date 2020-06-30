123fb365a45ac925f1888d229b340e086ba98d2a
Nami Shah <shah.nami@hotmail.com>
2020-06-30 21:35:53 +0100

    GH pages

--- next.config.js
+++ next.config.js
@@ -1,3 +1,9 @@
+const debug = process.env.NODE_ENV !== 'production';
 const withImages = require('next-images');
 
-module.exports = withImages();
+module.exports = withImages({
+	assetPrefix: !debug ? '' : '',
+	webpack(config, options) {
+		return config;
+	}
+});
