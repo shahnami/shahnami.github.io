3cb6319e8271f74ce1ceccc8001529595f4dfc1c
Nami Shah <shah.nami@hotmail.com>
2020-07-01 14:54:58 +0100

    source code

--- next.config.js
+++ next.config.js
@@ -1,9 +1,3 @@
-const debug = process.env.NODE_ENV !== 'production';
 const withImages = require('next-images');
 
-module.exports = withImages({
-	assetPrefix: !debug ? '' : '',
-	webpack(config, options) {
-		return config;
-	}
-});
+module.exports = withImages();
