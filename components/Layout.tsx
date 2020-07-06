34c3521e0ef6af9173554aa0c7cd4bf871ae0a15
Nami Shah <shah.nami@hotmail.com>
2020-07-06 20:28:36 +0100

    title page

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Head from 'next/head';
 
 import Footer from './Footer';
 import Header from './Header';
@@ -6,6 +7,13 @@ import Header from './Header';
 const Layout: React.FC<{}> = (props) => {
 	return (
 		<>
+			<Head>
+				<title>Nami Shah</title>
+				<meta
+					name="viewport"
+					content="initial-scale=1.0, width=device-width"
+				/>
+			</Head>
 			{/* <Header /> */}
 			<div>{props.children}</div>
 			{/* <Footer /> */}
